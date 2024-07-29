e37dc52d9aa17b3261707f3ca0c97050c944c9e7
Cassidy Pagels <rozeneclipse@gmail.com>
2024-07-29 02:18:36 -0500

    adding style persistence across pages

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,15 +1,33 @@
-const themeToggle = document.querySelector('#btnThemeToggle')
-const container = document.querySelector('.container')
+// declaring variables
+const currentTheme = localStorage.getItem('theme')
+const themeBtn = document.querySelector('#btnThemeToggle')
+const container = document.querySelector('body')
 
-let mode = 'light'
+// intercepts incoming theme from localstorage and sets it to initialize as 'light'
+if (currentTheme === null) {
+    localStorage.setItem('theme', 'light')
+}
 
-themeToggle.addEventListener('click', function () {
-    if (mode === 'light') {
-        mode = 'dark'
+let theme = currentTheme
+
+// sets theme of page on page load, persists across pages
+if (theme === 'dark') {
+    container.setAttribute('class', 'dark')
+}
+else {
+    container.setAttribute('class', 'light')
+}
+
+// function to make button both toggle the theme of the page and save the theme to localstorage for persistence
+themeBtn.addEventListener('click', function() {
+    if (theme != 'dark') {
+        theme = 'dark'
         container.setAttribute('class', 'dark')
+        localStorage.setItem('theme', 'dark')
     }
     else {
-        mode = 'light'
+        theme = 'light'
         container.setAttribute('class', 'light')
+        localStorage.setItem('theme', 'light')
     }
 })
